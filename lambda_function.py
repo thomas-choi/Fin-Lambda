7e1c99b4b0fa3f1c7c77ea0bf290ea17ed962631
Thomas Choi <thomas.choi@neuralmatrixllc.com>
2023-10-09 09:21:40 -0700

    dirplay working directory

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import os
 
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data = d)
     print(df)
-    print('Done x1.2')
+    print('Done x2.1')
+    current_working_directory = os.getcwd()
+    print('Current working directory', current_working_directory)
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        print(f)

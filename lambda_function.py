677b9734ab9e9100c3694828286402b857a976e1
Thomas Choi <thomas.choi@neuralmatrixllc.com>
2023-10-10 18:23:19 -0700

    Try to show the folder structure better

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import os
 
-def listDir(dir):
-    print('List directory: ', dir)
-    files = [f for f in os.listdir(dir) if os.path.isfile(f)]
-    for f in files:
-        print(f)  
-    namelist = [ f.name for f in os.scandir(dir) if f.is_dir() ]
-    print('Sub-Directories: ', namelist)
-    subfolders = [ f.path for f in os.scandir(dir) if f.is_dir() ]
-    print('Sub-Path: ', subfolders)
-    for folder in subfolders:
-        listDir(folder)
+def listDir(dir, level=50):
+    if level > 0:
+        level = level-1
+        print('List directory: ', dir)
+        files = [f for f in os.listdir(dir) if os.path.isfile(f)]
+        for f in files:
+            print(f)  
+        namelist = [ f.name for f in os.scandir(dir) if f.is_dir() ]
+        print('Sub-Directories: ', namelist)
+        subfolders = [ f.path for f in os.scandir(dir) if f.is_dir() ]
+        print('Sub-Path: ', subfolders)
+        for folder in subfolders:
+            listDir(folder)
 
 def env_display():
     d = {'col1': [1,2], 'col2': [3,4]}
@@ -20,7 +22,7 @@ def env_display():
     print('Done x2.1')
     current_working_directory = os.getcwd()
     listDir(current_working_directory)
-    listDir('/opt')
+    listDir('/opt', level=3)
 
 
 def lambda_handler(event, context):
